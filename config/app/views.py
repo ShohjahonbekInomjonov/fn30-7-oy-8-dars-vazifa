c12339d83097cb28d7ace39a7fd4557c36a93829
Shohjahon Inomjonov <inomjonovshohjahon790@gmail.com>
2025-10-01 16:43:20 +0500

    Update views.py

--- config/app/views.py
+++ config/app/views.py
@@ -3,9 +3,24 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from .models import Car, Brand
 from rest_framework import status
-from .serializers import CarSerializer
+from .serializers import CarSerializer, BrandSerializer
 from django.db import transaction
 from django.core.exceptions import ValidationError
+
+
+class BrandAPIView(APIView):
+    def get(self, request: Request, pk: Optional[int]=None):
+        if not pk:
+            brands = Brand.objects.all()
+            serializer = BrandSerializer(brands, many=True)
+            return Response(serializer.data)
+        else:
+            try:
+                brand = Brand.objects.get(pk=pk)
+                serializer = BrandSerializer(brand)
+                return Response(serializer.data)
+            except Exception as e:
+                return Response({"message": "Brand Not Found !!!"}, status=status.HTTP_404_NOT_FOUND)
 
 
 class CarAPIView(APIView):
